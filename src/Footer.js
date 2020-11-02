157bea62399a828ef31261b37c5c3f4aa7c0f831
vistritpandey <vistritpandey@gmail.com>
2020-11-02 14:15:01 +0530

    m

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
 import './Footer.css'
 
-function () {
+function Footer() {
     return (
         <div className='footer'>
-            
+            <p>© 2020 Vistrit's Airbnb clone!</p>
+            <p>Privacy · Terms · Sitemap · Company Details</p>
         </div>
     )
 }
 
-export default 
+export default Footer
