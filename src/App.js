076c9328fea35b419ad656d43436c6dc3a99e4b6
vistritpandey <vistritpandey@gmail.com>
2020-11-02 14:02:42 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './Header'
+import Banner from './Banner'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <Header />
+      <Banner />
     </div>
   );
 }
---
+++ src/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+    return (
+        <div className="banner">
+            <h5>I'm the banner</h5>
+        </div>
+    )
+}
+
+export default Banner
