27c1bbeec4a4def8b223dfeac277a70aebb13b70
vistritpandey <vistritpandey@gmail.com>
2020-11-02 14:52:28 +0530

    Complete

--- src/Banner.js
+++ src/Banner.js
@@ -5,7 +5,6 @@ import Search from './Search';
 import { useHistory } from "react-router-dom";
 
 function Banner() {
-    const history = useHistory();
     const [showSearch, setShowSearch] = useState(false);
 
     return (
@@ -17,13 +16,7 @@ function Banner() {
                     {showSearch ? "Hide" : "Search Dates"}
                 </Button>
             </div>
-            <div className='banner__info'>
-                <h1>Get out and stretch your imagination</h1>
-                <h5>
-                    Plan a different kind of getaway to uncover the hidden gems near you.
-                </h5>
-                <Button onClick={() => history.push('/search')} variant='outlined'>Explore Nearby</Button>
-            </div>
+            
         </div>
     )
 }
